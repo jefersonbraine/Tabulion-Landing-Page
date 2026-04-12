83b9e7ec61a01380ad7f8eaf5e975cc752e4bee3
Jeferson <jefeleal363@gmail.com>
2026-04-12 15:21:43 -0300

    fix: testes

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Tabulion_RPA_Landing_Page');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Test testando');
   });
 });
